45979afbc5c3bcc2531b8824096a0ad0570b307e
Anton Asmirko <AntonAsmirko@yandex.ru>
2020-11-07 14:44:06 +0300

    .

--- app/src/main/java/com/example/starsystem/views/StarSystemView.kt
+++ app/src/main/java/com/example/starsystem/views/StarSystemView.kt
@@ -3,9 +3,8 @@ package com.example.starsystem.views
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.*
+import android.graphics.Canvas
 import android.graphics.drawable.Drawable
-import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.util.AttributeSet
@@ -19,10 +18,6 @@ import kotlin.math.sin
 @SuppressLint("CustomViewStyleable")
 class StarSystemView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    companion object {
-        const val SUPER_STATE = "superState"
-    }
-
     var isInfiniteAnimation = false
 
     var animationDuration = 0L
@@ -31,13 +26,16 @@ class StarSystemView(context: Context, attrs: AttributeSet) : View(context, attr
             startAnimationTime = System.currentTimeMillis()
             invalidate()
         }
-
     var startAnimationTime = -1L
+
+    private var namesOfPlanets: java.util.ArrayList<String> = ArrayList()
 
     var planets: List<Planet>? = null
         set(list) {
             field = list
-            field!!.forEach { it.view = this }
+            field?.forEach { it.view = this }
+            if (namesOfPlanets.isEmpty())
+                field?.forEach { namesOfPlanets.add(it.name) }
         }
 
     init {
@@ -77,15 +75,83 @@ class StarSystemView(context: Context, attrs: AttributeSet) : View(context, attr
         }
     }
 
-    override fun onSaveInstanceState(): Parcelable? {
-        val bundle = Bundle()
-        bundle.putParcelable(SUPER_STATE, super.onSaveInstanceState())
-
-        return super.onSaveInstanceState()
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        this.namesOfPlanets = ArrayList<String>().apply { addAll(savedState.namesOfThePlanets) }
+        this.planets = savedState.planets
+        this.isInfiniteAnimation = savedState.isInfiniteAnimation
+        this.animationDuration = savedState.animDuration
+        this.startAnimationTime = savedState.startAnimTime
     }
 
-    override fun onRestoreInstanceState(state: Parcelable?) {
-        super.onRestoreInstanceState(state)
+    override fun onSaveInstanceState(): Parcelable? {
+        return SavedState(
+            super.onSaveInstanceState(),
+            namesOfPlanets.toArray(Array(namesOfPlanets.size) { "a" }),
+            planets as MutableList<Planet>,
+            isInfiniteAnimation,
+            animationDuration,
+            startAnimationTime
+        )
+    }
+
+    class SavedState : BaseSavedState {
+
+        var namesOfThePlanets: Array<String>
+        var planets: MutableList<Planet>? = null
+        var isInfiniteAnimation = false
+        var animDuration = -1L
+        var startAnimTime = -1L
+
+        constructor(
+            superState: Parcelable?,
+            namesOfThePlanets: Array<String>,
+            planets: MutableList<Planet>?,
+            isInfiniteAnimation: Boolean,
+            animDuration: Long,
+            startAnimTime: Long
+        ) : super(superState) {
+            this.namesOfThePlanets = namesOfThePlanets
+            this.planets = planets
+            this.isInfiniteAnimation = isInfiniteAnimation
+            this.animDuration = animDuration
+            this.startAnimTime = startAnimTime
+        }
+
+        constructor(superState: Parcel?) : super(superState){
+            val arrSize = superState!!.readInt()
+            this.namesOfThePlanets = Array(arrSize){"a"}
+            superState.readStringArray(this.namesOfThePlanets)
+            this.planets = mutableListOf()
+            for (i in 0 until arrSize)
+                planets!!.add(superState.readParcelable(Planet.javaClass.classLoader)!!)
+            this.isInfiniteAnimation = superState.readInt() == 1
+            this.animDuration = superState.readLong()
+            this.startAnimTime = superState.readLong()
+        }
+
+        override fun writeToParcel(out: Parcel?, flags: Int) {
+            super.writeToParcel(out, flags)
+            out!!.writeInt(namesOfThePlanets.size)
+            out.writeArray(namesOfThePlanets)
+            planets?.forEach { out.writeParcelable(it, 0) }
+            out.writeInt(if (isInfiniteAnimation) 1 else 0)
+            out.writeLong(animDuration)
+            out.writeLong(startAnimTime)
+        }
+
+        companion object CREATOR : Parcelable.Creator<SavedState?> {
+            override fun createFromParcel(source: Parcel?): SavedState? {
+                return SavedState(source)
+            }
+
+
+            override fun newArray(size: Int): Array<SavedState?> {
+                return Array(size) { null }
+            }
+
+        }
     }
 
     class Planet(
@@ -103,7 +169,8 @@ class StarSystemView(context: Context, attrs: AttributeSet) : View(context, attr
         var view: View? = null
             set(v) {
                 field = v
-                image = AppCompatResources.getDrawable(v!!.context, imageId)!!
+                if (v != null)
+                    image = AppCompatResources.getDrawable(v.context, imageId)!!
             }
 
         var parent: Planet? = null
